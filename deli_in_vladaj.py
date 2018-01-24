e96aa9855dde944ecfbbdfa0bcf5a24edf0573f4
SashaOslaj <sasa.oslaj@student.fmf.uni-lj.si>
2018-01-25 00:51:14 +0100

    Add files via upload

---
+++ deli_in_vladaj.py
@@ -0,0 +1,110 @@
+##########################################################################
+# Želimo definirati pivotiranje na mestu za tabelo a.
+# Ker bi želeli pivotirati zgolj dele tabele a, se hkrati omejimo na
+# del tabele, ki se nahaja med indeksoma start in end.
+# Na primer, za start = 0 in end = 8 tabelo
+#
+# [10, 4, 5, 15, 11, 2, 17, 0, 18]
+#
+# preuredimo v
+#
+# [0, 2, 5, 4, 10, 11, 17, 15, 18]
+#
+# (Možnih je več različnih rešitev, pomembno je, da je element 10 pivot.)
+#
+# Sestavi funkcijo pivot_list(a, start, end), ki preuredi tabelo a tako,
+# da bo a[start] postal pivot za del tabele med indeksoma start in end.
+# Funkcija naj vrne indeks, na katerem je po preurejanju pristal pivot.
+# Funkcija naj deluje v času O(n), kjer je n dolžina tabele a.
+# Primer:
+#
+#     >>> a = [10, 4, 5, 15, 11, 2, 17, 0, 18]
+#     >>> pivot_list(a, 1, 7)
+#     3
+#     >>> a
+#     [10, 2, 0, 4, 11, 15, 17, 5, 18]
+##########################################################################
+
+def pivot_list(a, start, end) :
+    # Shrani pivot
+    pivot = a[start]
+    # Shrani kazalca
+    front_i = start
+    back_i = end
+    # Premikaj kazalca in zamenjaj elemente če potrebno
+    while front_i != back_i:
+        if a[front_i + 1] <= pivot:
+            front_i += 1
+        elif a[back_i] > pivot:
+            back_i -= 1
+        else:
+            temp = a[front_i + 1]
+            a[front_i + 1] = a[back_i]
+            a[back_i] = temp
+    # Premakni pivot na pravo mesto
+    a[start] = a[front_i]
+    a[front_i] = pivot
+    # Vrni indeks pivota
+    return front_i
+
+##########################################################################
+# Tabelo a želimo urediti z algoritmom hitrega urejanja, ki smo ga
+# spoznali na predavanjih.
+#
+# Napišite funkcijo quicksort(a), ki uredi tabelo a s pomočjo pivotiranja.
+# Poskrbi, da algoritem deluje 'na mestu', torej ne uporablja novih tabel.
+#
+# Namig: definirajte pomožno funkcijo quicksort_part(a, start, end), ki
+#        uredi zgolj del tabele a.
+#
+#   >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+#   >>> quicksort(a)
+#   [2, 3, 4, 5, 10, 11, 15, 17, 18]
+##########################################################################
+
+def quicksort_part(a, start, end):
+    if start >= end:
+        return
+    else:
+        pivot_i = pivot_list(a, start, end)
+        quicksort_part(a, start, pivot_i - 1)
+        quicksort_part(a, pivot_i + 1, end)
+        return
+
+def quicksort(a):
+    quicksort_part(a, 0, len(a) - 1)
+    return
+
+##########################################################################
+# V tabeli a želimo poiskati vrednost k-tega elementa po velikosti.
+# Na primer, če je
+#
+# >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+#
+# potem je tretji element po velikosti enak 5, ker so od njega manši
+# elementi 2, 3 in 4. Pri tem štejemo indekse od 0 naprej, se pravi
+# "ničti" element je 2.
+#
+# Sestavite funkcijo kth_element(a, k), ki v tabeli a poišče k-ti element
+# po velikosti. Funkcija sme spremeniti tabelo a.
+#
+# Namig: ponovno si pomagaj s pomožno funkcijo.
+##########################################################################
+
+def kth_el_part(a, k, start, end):
+    if start > end:
+        return None
+    else:
+        pivot_i = pivot_list(a, start, end)
+        if pivot_i == k:
+            return a[pivot_i]
+        elif pivot_i > k:
+            return kth_el_part(a, k, start, pivot_i - 1)
+        else:
+            return kth_el_part(a, k, pivot_i + 1, end)
+
+def kth_element(a, k):
+    if k>len(a):
+        return None
+    else:
+        return kth_el_part(a, k, 0, len(a)-1)
---
+++ urejanje.ml
@@ -0,0 +1,126 @@
+(* Konstruiraj seznam dolžine "len" z naključnimi vrednostmi med 0 in "max". 
+
+   Primer:
+
+   utop[1]> let l = randlist 10 10 ;;
+   val l : int list = [0; 1; 0; 4; 0; 9; 1; 2; 5; 4]
+
+   S pomočjo te funkcije lahko kasneje testiraš algoritme za urejanje.
+   let l = (randlist 100 100) in selection_imperative_list l = List.sort compare l;;
+ *)
+let rec randlist len max = 
+  match len with
+  | 0 -> []
+  | len -> (Random.int max) :: randlist (len - 1) max
+
+(* Funkcija "insert y xs" vstavi "y" v že urejen seznam "xs" in vrne posodobljen
+   urejen seznam.
+   
+   Primer:
+
+   utop[75]> insert 9 [0; 2];;
+   - : int list = [0; 2; 9]
+   utop[76]> insert 1 [0; 2];;
+   - : int list = [0; 1; 2]
+   utop[79]> insert 1 [];;
+   - : int list = [1]
+ *)
+let rec insert y xs = 
+  match xs with
+  | [] -> [y]
+  | hd :: tl ->
+    if hd < y 
+	then hd :: insert y tl
+	else y :: hd :: tl
+
+(* Prazen seznam je urejen. Seznam lahko uredimo tako, da pravilno vstavljamo
+   vse elemente seznama v prazen seznam. S pomočjo te ideje napiši funkcijo
+   "insertion_sort" z uporabo "List.fold_left" in "insert". *)
+let rec ins_sort l = 
+  match l with
+  | [] -> []
+  | hd :: tl -> insert hd (ins_sort tl)
+
+let ins_sort2 l = List.fold_left (fun acc x -> insert x acc) [] l
+
+(* Napiši rekurzivno funkcijo, ki sprejme seznam "l" in v primeru, ko "l"
+   ni prazen seznam, vrne par "Some (z, l_without_z)" pri čemer je "z" 
+   najmanjši element seznama "l" in "l_without_z" ne vsebuje prve ponovitve
+   elementa "z". *)
+
+let rec min_and_rest l = 
+  match l with 
+  | [] -> None
+  | x :: xs -> 
+    let rec odstrani_prvo z = function 
+       | [] -> failwith "ni najdu" 
+       | x :: xs -> if x = z then xs else z :: odstrani_prvo z xs in 
+     let rec aux y = function 
+       | [] -> y 
+       | x::xs -> aux (min x y) xs 
+     in 
+     let z = aux x xs in 
+     Some (z, odstrani_prvo z l) 
+
+(* Urejanje z izbiranjem poteka tako, da hranimo seznam "l" ločen v 
+   dveh podseznamih. Prvi seznam vsebuje že urejen del seznama, drugi 
+   pa vse elemente, ki jih je še potrebno urediti.
+   Nato zaporedno izmed še neurejenih elementov izbiramo najmanjšega in
+   ga dodamo urejenemu podseznamu. *)
+
+(* Z uporabo "min_and_rest" napiši rekurzivno funkcijo, ki ureja z 
+   izbiranjem. *)
+let rec selection_sort l = 
+  let rec aux sorted xs = 
+        match min_and_rest xs with 
+        | None -> List.rev sorted 
+        | Some (x,xs) -> aux (x::sorted) xs 
+      in aux [] l 
+
+(* Pri delu z tabelami (array) namesto seznami, lahko urejanje z izbiranjem
+   naredimo "na mestu", t.j. brez uporabe vmesnih kopij (delov) vhoda.
+   Kot prej tabelo ločujemo na že urejen del in še neurejen del, le da 
+   tokrat vse elemente hranimo v vhodni tabeli, mejo med deloma pa hranimo
+   v spremenljivki (npr. "boundary_sorted").
+   Na vsakem koraku poiščemo indeks najmanjšega elementa neurejenega dela
+   tabele in ga zamenjamo z elementom na meji med deloma (dodamo na konec
+   urejenega dela). Postopek končamo, ko meja doseže konec tabele. *)
+
+(* Napiši funkcijo "swap a i j", ki zamenja "a.(i)" in "a.(j)". *)
+let swap a i j = 
+  let t = a.(i) in 
+  a.(i) <- a.(j); a.(j) <- t 
+
+
+(* Napiši funkcijo "index_min a lower upper", ki izračuna index najmanjšega
+   elementa v "a" med indeksoma "lower" in "upper".
+   
+   Primer:
+   
+   index_min [|0; 2; 9; 3; 6|] 2 4 = 4 
+*)
+let index_min a lower upper = 
+  let index_min = ref lower in 
+     for i = lower to upper do 
+       if a.(i) < a.(!index_min) then 
+         index_min := i 
+     done; 
+     !index_min 
+
+
+(* Konstruiraj urejanje z izbiro na mestu. *)
+let selection_imperative a = 
+  let index_end = Array.length a - 1 in 
+   for boundary_sorted = 0 to index_end do 
+      let i = index_min a boundary_sorted index_end in 
+     swap a i boundary_sorted 
+   done 
+
+
+(* Za testiranje urejanja tabel lahko funkcijo pretvoriš v funkcijo, ki 
+   ureja sezname z uporabo "Array.of_list" in "Array.to_list". *)
+let selection_imperative_list l = 
+  let a = Array.of_list l in 
+      selection_imperative a; 
+     Array.to_list a 
+
